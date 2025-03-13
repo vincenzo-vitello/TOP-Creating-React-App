a65f5de22840619e998573e86fd7159b3f26050f
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-03-13 11:45:54 +0100

    playing with useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,13 @@ import Greeting from './components/Greetings'
 import Animals from './components/Animals'
 import Button from './components/Button'
 
-const handleButtonClick = (url) => {
-  window.location.href = url;
-};
 
 function App() {
   return (
    <>
     <Greeting/>
     <Animals />
-    <Button text="don't click me" color={'red'} fontSize={20}/>
-    <Button text="click me" color={'green'} fontSize={18}/>
     <Button />
-    <Button text="Google" handleClick={() => handleButtonClick("https://www.google.com")}/>
    </>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,33 @@
-const Button = ({ text = 'Clicky', color = "blue", fontSize = 25, handleClick}) => {
-    const buttonStyle = {
-        color: color,
-        fontSize: fontSize + 'px'
-    }
+import { useState } from "react";
+const COLORS = ['pink', 'brown', 'red']
 
-    return <button style={buttonStyle} onClick={handleClick} >{text}</button>
+const Button = () => {
+    const [backgroundColor, setBackgroundColor] = useState(COLORS[0])
+
+    const handleBtnClick = (color) => {
+        setBackgroundColor(color)
+    }
+    const body = document.querySelector('body')
+    body.style.backgroundColor = backgroundColor
+
+    return (
+       <>
+            <div className="button">
+                {COLORS.map((color) => {
+                    return (
+                        <button 
+                            type="button" 
+                            key={color} 
+                            onClick={() => handleBtnClick(color)} className={backgroundColor === color ? 'selected' : ''}
+                            >
+                            {color}
+                        </button>
+                    )
+                })}
+            </div>
+            <div>Background color is: {backgroundColor}</div>
+       </>
+    )
 }
 
 export default Button
